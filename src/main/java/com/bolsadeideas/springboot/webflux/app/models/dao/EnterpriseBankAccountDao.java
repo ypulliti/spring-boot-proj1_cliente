c40d23026d1973c43f00823b77c2673a5eaf63b1
Yelinna Beatriz Pulliti Carrasco <ypulliti@emeal.nttdata.com>
2022-08-09 19:48:10 -0500

    added crud functions

--- src/main/java/com/bolsadeideas/springboot/webflux/app/models/dao/EnterpriseBankAccountDao.java
+++
@@ -1,8 +0,0 @@
-package com.bolsadeideas.springboot.webflux.app.models.dao;
-
-import com.bolsadeideas.springboot.webflux.app.models.documents.EnterpriseBankAccount;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface EnterpriseBankAccountDao extends ReactiveMongoRepository<EnterpriseBankAccount, String>{
-
-}
